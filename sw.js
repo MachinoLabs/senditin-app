6d685c902d51de2dc1b7c4a70bdc02037e504ec5
MachinoLabs <amateuraiguy@gmail.com>
2026-03-30 20:04:47 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,30 @@
-const CACHE_NAME = 'send-it-in-v7-FINAL';
-const urlsToCache = ['./', './index.html', './manifest.json', './sw.js'];
+const CACHE_NAME = 'send-it-in-v8';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './sw.js'
+];
 
 self.addEventListener('install', event => {
   self.skipWaiting(); 
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))
+      );
+    })
+  );
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
 });
