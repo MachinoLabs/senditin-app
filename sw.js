e5178de56154ecea425d07c575fc638d3809a8dd
MachinoLabs <amateuraiguy@gmail.com>
2026-04-02 12:08:45 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,13 @@
-const CACHE_NAME = 'send-it-in-v11';
+const CACHE_NAME = 'send-it-in-universal-v1';
 const urlsToCache = [
   './',
   './index.html',
-  './manifest.json',
-  './sw.js'
+  './manifest.json'
 ];
 
 self.addEventListener('install', event => {
-  self.skipWaiting(); 
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))
-      );
-    })
   );
 });
 
