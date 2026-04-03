b848522b4558008fd79e812725a2ce7af2f3c018
MachinoLabs <amateuraiguy@gmail.com>
2026-04-03 18:09:59 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,39 @@
-const CACHE_NAME = 'send-it-in-universal-v1';
-const urlsToCache = [
+const CACHE_NAME = 'send-it-in-v5'; // Update this number whenever you change code!
+const ASSETS = [
   './',
   './index.html',
-  './manifest.json'
+  './manifest.json',
+  './icon-192.png'
 ];
 
-self.addEventListener('install', event => {
+// Force immediate installation
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting(); 
+});
+
+// Cleanup old caches and claim control
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)));
+    })
+  );
+  self.clients.claim();
+});
+
+// Offline-first fetch strategy
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
   );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
+// Listen for the "Update" message from the UI toast
+self.addEventListener('message', (event) => {
+  if (event.data.action === 'skipWaiting') {
+    self.skipWaiting();
+  }
 });
