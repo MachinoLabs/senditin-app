77edbfaefc8b5e4ecb44bd2c6a174d5aabac1040
MachinoLabs <amateuraiguy@gmail.com>
2026-04-20 14:42:58 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE_NAME = 'send-it-in-v1';
+// Change this string every time you update your app! (e.g., v2.7, v2.8)
+const CACHE_NAME = 'master-brain-cache-v2.6';
 const urlsToCache = [
   './',
   './index.html',
@@ -6,8 +7,31 @@ const urlsToCache = [
 ];
 
 self.addEventListener('install', event => {
+  // Force the waiting service worker to become the active service worker immediately
+  self.skipWaiting();
+
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// --- NEW: THE CACHE JANITOR ---
+self.addEventListener('activate', event => {
+  // Tell the active service worker to take control of the page immediately
+  event.waitUntil(self.clients.claim());
+
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          // If the cache name doesn't match our current version, nuke it
+          if (cacheName !== CACHE_NAME) {
+            console.log('Clearing old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
 
